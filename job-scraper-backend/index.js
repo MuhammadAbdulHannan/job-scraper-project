a617567c349e5aac2d78ae48a04233327396edc9
Muhammad Abdul Hannan <hannanryk2@gmail.com>
2026-09-02 17:00:54 +0500

    Initial project structure - backend and frontend

---
+++ job-scraper-backend/index.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        status: "Backend Running"
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port http://localhost:${process.env.Port}`)
+});
+
